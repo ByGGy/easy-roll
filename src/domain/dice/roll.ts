f164af85c5378700a8240b657c205cfd388185ad
BMA <benjamin.manderscheid@analogway.com>
2024-10-17 11:36:49 +0200

    corrige le test sur les rolls
    et ajoute un peu d'info

--- src/domain/dice/roll.ts
+++ src/domain/dice/roll.ts
@@ -1,7 +1,8 @@
 export const rollDice = (max: number) => 1 + Math.floor(Math.random() * max)
 
+// cf Chi-square distribution table
 export const runTest = () => {
-  const sampleQty = 100000
+  const sampleQty = 10000
   const diceMaxValue = 8
   const results: Record<number, number> = {}
 
@@ -14,13 +15,13 @@ export const runTest = () => {
     }
   }
 
-  const perfectQtyPerValue = sampleQty / 8
+  const perfectQtyPerValue = sampleQty / diceMaxValue
   const gapPerValue = Object.keys(results).reduce((acc, key) => {
     acc[key] = ((results[key] - perfectQtyPerValue) * (results[key] - perfectQtyPerValue)) / perfectQtyPerValue
     return acc
   }, {} as Record<number, number>)
 
-  const overallGap = Object.values(gapPerValue).reduce((acc, value) => acc += value, 0)
+  const overallGap = Object.values(gapPerValue).reduce((acc, value) => acc + value, 0)
 
   console.log(results)
   console.log(gapPerValue)
