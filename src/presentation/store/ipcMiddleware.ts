23356273dadc842c27e99b9b56cd7f222f680dff
BMA <benjamin.manderscheid@analogway.com>
2024-11-14 18:33:49 +0100

    sélectionne automatiquement le dernier perso ajouté dans une session

--- src/presentation/store/ipcMiddleware.ts
+++ src/presentation/store/ipcMiddleware.ts
@@ -3,6 +3,9 @@ import { Middleware, UnknownAction } from 'redux'
 import { updateCollection as updateCharacterCollection, updateCharacter } from './characterCollectionSlice'
 import { updateCollection as updateSessionCollection, updateSession } from './sessionCollectionSlice'
 import { add as addToHistory } from './rollHistorySlice'
+import { pickCharacter } from './selectionSlice'
+
+import { EntityId } from '../../domain/common/types'
 
 export const ipcMiddleware: Middleware = (store) => {
   window.electronAPI.onMessage('Domain.CharacterRepository.update', (data: string) => {
@@ -24,6 +27,16 @@ export const ipcMiddleware: Middleware = (store) => {
   window.electronAPI.onMessage('Domain.Session.update', (data: string) => {
     const states = JSON.parse(data)
     store.dispatch(updateSession(states))
+
+    // Auto select a new character added to the session
+    const oldCharacterIds: Array<EntityId> = states.previous.characterIds
+    const newCharacterIds: Array<EntityId> = states.current.characterIds
+    if (newCharacterIds.length > oldCharacterIds.length) {
+      const newCharacterId = newCharacterIds.find(id => !oldCharacterIds.includes(id))
+      if (newCharacterId) {
+        store.dispatch(pickCharacter(newCharacterId))
+      }
+    }
   })
 
   window.electronAPI.onMessage('Domain.Roll.new', (data: string) => {
