43b3e0c1fef9a5fd751ccc5789f2e0e593c24a1d
BMA <benjamin.manderscheid@analogway.com>
2025-05-27 12:28:18 +0200

    ajoute un composant permettant de visualiser un pourcentage de succès

---
+++ src/presentation/components/common/shutter-icon.tsx
@@ -0,0 +1,17 @@
+import { SvgIcon, SvgIconProps } from '@mui/material'
+
+export const ShutterIcon = (props: SvgIconProps) => {
+  return (
+    <SvgIcon {...props}>
+      <svg viewBox='0 0 153 144' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <circle cx='75.9061' cy='71.4735' r='66.5411' stroke='white'/>
+        <path d='M79.2305 12.1892L93.636 37.1219H27.1489C27.1489 37.1219 37.676 24.3785 49.8653 18.2839C62.0546 12.1892 79.2305 12.1892 79.2305 12.1892Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+        <path d='M26.2402 39.0384L55.035 39.1567L21.5369 96.5884C21.5369 96.5884 15.833 81.0746 16.7098 67.4748C17.5865 53.8749 26.2402 39.0384 26.2402 39.0384Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+        <path d='M126.309 104.115L97.5144 104.151L130.705 46.5409C130.705 46.5409 136.492 62.0241 135.688 75.6284C134.884 89.2327 126.309 104.115 126.309 104.115Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+        <path d='M129.634 45.1039L115.244 70.0458L82.0007 12.4663C82.0007 12.4663 98.3003 15.2114 109.673 22.7203C121.046 30.2292 129.634 45.1039 129.634 45.1039Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+        <path d='M73.5117 130.678L58.8902 105.871L125.375 105.294C125.375 105.294 114.959 118.128 102.823 124.328C90.6869 130.528 73.5117 130.678 73.5117 130.678Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+        <path d='M22.8246 98.2018L36.997 73.1359L70.7393 130.425C70.7393 130.425 54.4165 127.821 42.9789 120.411C31.5414 113.001 22.8246 98.2018 22.8246 98.2018Z' fill='currentColor' stroke='currentColor' strokeWidth='0' />
+      </svg>
+    </SvgIcon>
+  )
+}
---
+++ src/presentation/components/common/success-rate.tsx
@@ -0,0 +1,83 @@
+import { get } from 'lodash'
+import Color from 'color'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography, { TypographyOwnProps } from '@mui/material/Typography'
+import { useTheme } from '@mui/material'
+import { darken } from '@mui/material/styles'
+
+import { ShutterIcon } from './shutter-icon'
+
+type HexColor = `#${string}`
+
+type OutlinedTextProps = {
+  text: string
+  variant?: TypographyOwnProps['variant']
+  color?: HexColor
+}
+
+const OutlinedText = ({ text, variant, color }: OutlinedTextProps) => {
+  return (
+    <Box position="relative" display="inline-block">
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        sx={{ zIndex: 0 }}
+      >
+        <Typography variant={variant}
+          sx={{
+            color: 'transparent',
+            fontWeight: 'bold',
+            WebkitTextStrokeWidth: '0.25em',
+            WebkitTextStrokeColor: `${darken(color ?? '#000000', 0.65)}`,
+          }}
+        >
+          {text}
+        </Typography>
+      </Box>
+
+      <Typography variant={variant} color={color}
+        sx={{
+          position: 'relative',
+          fontWeight: 'bold',
+          zIndex: 1,
+        }}
+      >
+        {text}
+      </Typography>
+    </Box>
+  )
+}
+
+type Props = {
+  successRatio: number
+}
+
+export const SuccessRate = ({ successRatio = 0.64 }: Props) => {
+  const theme = useTheme()
+  const warningColor = get(theme.palette, 'warning.main')
+  const successColor = get(theme.palette, 'success.main')
+  const targetColor = Color(warningColor).mix(Color(successColor), successRatio).hex() as HexColor
+
+  return (
+    <Box position="relative" width={120} height={120}>
+      <ShutterIcon opacity={0.15} color='disabled' sx={{
+          position: 'absolute',
+          top: '0%',
+          left: '0%',
+          width: '100%',
+          height: '100%',
+        }} />
+
+      <Stack direction='row' alignItems='baseline' margin={4} sx={{
+          position: 'absolute',
+          top: '-10%',
+          left: '10%',
+        }}>
+        <OutlinedText text={`${Math.round(successRatio * 100)}`} variant='h3' color={targetColor} />
+        <OutlinedText text='%' variant='h6' color={targetColor} />
+      </Stack>
+    </Box>
+  )
+}
