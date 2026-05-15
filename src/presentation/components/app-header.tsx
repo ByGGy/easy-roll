922a8896d4a963d3e1903b9839875bcd22016e0b
BMA <benjamin.manderscheid@analogway.com>
2026-05-15 19:48:39 +0200

    affiche le logo de l'app ds le header

--- src/presentation/components/app-header.tsx
+++ src/presentation/components/app-header.tsx
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootState, AppDispatch } from '../store/store'
 import { closeSession } from '../store/selectionSlice'
 
-import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import LogoutIcon from '@mui/icons-material/Logout'
 
+import { EzRollIcon } from './common/ezRoll-icon'
 import { VersionInfo } from './version-info'
 
 export const AppHeader = () => {
@@ -23,13 +24,14 @@ export const AppHeader = () => {
   return (
     <AppBar position='static' color='transparent' enableColorOnDark>
       <Toolbar>
-        <Box flexGrow={1}>
+        <Stack direction='row' alignItems='center' flex={1}>
+          <EzRollIcon sx={{ width: '4em', color: (theme) => theme.palette.text.disabled, mr: 4 }} />
           {isSessionSelected &&
             <Button variant='outlined' startIcon={<LogoutIcon />} onClick={handleCloseSession}>
               Back
             </Button>
           }
-        </Box>
+        </Stack>
         <VersionInfo />
       </Toolbar>
     </AppBar>
---
+++ src/presentation/components/common/ezRoll-icon.tsx
@@ -0,0 +1,178 @@
+import { SvgIcon, SvgIconProps } from '@mui/material'
+
+export const EzRollIcon = ({ strokeWidth = 2, ...others }: SvgIconProps) => {
+  return (
+    <SvgIcon strokeWidth={strokeWidth} {...others}>
+      <svg width="775" height="197" viewBox="0 0 775 197" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="29" cy="50" r="8" fill='currentColor' />
+        <circle cx="8" cy="70" r="8" fill='currentColor' />
+        <circle cx="29" cy="70" r="8" fill='currentColor' />
+        <circle cx="50" cy="50" r="8" fill='currentColor' />
+        <circle cx="71" cy="50" r="8" fill='currentColor' />
+        <circle cx="50" cy="70" r="8" fill='currentColor' />
+        <circle cx="71" cy="70" r="8" fill='currentColor' />
+        <circle cx="92" cy="50" r="8" fill='currentColor' />
+        <circle cx="92" cy="70" r="8" fill='currentColor' />
+        <circle cx="113" cy="70" r="8" fill='currentColor' />
+        <circle cx="152" cy="50" r="8" fill='currentColor' />
+        <circle cx="152" cy="70" r="8" fill='currentColor' />
+        <circle cx="173" cy="50" r="8" fill='currentColor' />
+        <circle cx="194" cy="50" r="8" fill='currentColor' />
+        <circle cx="173" cy="70" r="8" fill='currentColor' />
+        <circle cx="194" cy="70" r="8" fill='currentColor' />
+        <circle cx="215" cy="50" r="8" fill='currentColor' />
+        <circle cx="236" cy="50" r="8" fill='currentColor' />
+        <circle cx="215" cy="70" r="8" fill='currentColor' />
+        <circle cx="236" cy="70" r="8" fill='currentColor' />
+        <circle cx="257" cy="50" r="8" fill='currentColor' />
+        <circle cx="257" cy="70" r="8" fill='currentColor' />
+        <circle cx="296" cy="50" r="8" fill='currentColor' />
+        <circle cx="296" cy="70" r="8" fill='currentColor' />
+        <circle cx="317" cy="50" r="8" fill='currentColor' />
+        <circle cx="317" cy="70" r="8" fill='currentColor' />
+        <circle cx="380" cy="50" r="8" fill='currentColor' />
+        <circle cx="380" cy="70" r="8" fill='currentColor' />
+        <circle cx="401" cy="50" r="8" fill='currentColor' />
+        <circle cx="401" cy="70" r="8" fill='currentColor' />
+        <circle cx="645" cy="48" r="8" fill='currentColor' />
+        <circle cx="645" cy="68" r="8" fill='currentColor' />
+        <circle cx="666" cy="48" r="8" fill='currentColor' />
+        <circle cx="666" cy="68" r="8" fill='currentColor' />
+        <circle cx="726" cy="48" r="8" fill='currentColor' />
+        <circle cx="726" cy="68" r="8" fill='currentColor' />
+        <circle cx="747" cy="48" r="8" fill='currentColor' />
+        <circle cx="747" cy="68" r="8" fill='currentColor' />
+        <circle cx="296" cy="10" r="8" fill='currentColor' />
+        <circle cx="296" cy="30" r="8" fill='currentColor' />
+        <circle cx="317" cy="10" r="8" fill='currentColor' />
+        <circle cx="338" cy="10" r="8" fill='currentColor' />
+        <circle cx="317" cy="30" r="8" fill='currentColor' />
+        <circle cx="338" cy="30" r="8" fill='currentColor' />
+        <circle cx="359" cy="10" r="8" fill='currentColor' />
+        <circle cx="380" cy="10" r="8" fill='currentColor' />
+        <circle cx="359" cy="30" r="8" fill='currentColor' />
+        <circle cx="380" cy="30" r="8" fill='currentColor' />
+        <circle cx="401" cy="30" r="8" fill='currentColor' />
+        <circle cx="645" cy="8" r="8" fill='currentColor' />
+        <circle cx="645" cy="28" r="8" fill='currentColor' />
+        <circle cx="666" cy="8" r="8" fill='currentColor' />
+        <circle cx="666" cy="28" r="8" fill='currentColor' />
+        <circle cx="726" cy="8" r="8" fill='currentColor' />
+        <circle cx="726" cy="28" r="8" fill='currentColor' />
+        <circle cx="747" cy="8" r="8" fill='currentColor' />
+        <circle cx="747" cy="28" r="8" fill='currentColor' />
+        <circle cx="8" cy="169" r="8" fill='currentColor' />
+        <circle cx="29" cy="169" r="8" fill='currentColor' />
+        <circle cx="29" cy="189" r="8" fill='currentColor' />
+        <circle cx="50" cy="169" r="8" fill='currentColor' />
+        <circle cx="71" cy="169" r="8" fill='currentColor' />
+        <circle cx="50" cy="189" r="8" fill='currentColor' />
+        <circle cx="71" cy="189" r="8" fill='currentColor' />
+        <circle cx="92" cy="169" r="8" fill='currentColor' />
+        <circle cx="113" cy="169" r="8" fill='currentColor' />
+        <circle cx="92" cy="189" r="8" fill='currentColor' />
+        <circle cx="152" cy="169" r="8" fill='currentColor' />
+        <circle cx="152" cy="189" r="8" fill='currentColor' />
+        <circle cx="173" cy="169" r="8" fill='currentColor' />
+        <circle cx="194" cy="169" r="8" fill='currentColor' />
+        <circle cx="173" cy="189" r="8" fill='currentColor' />
+        <circle cx="194" cy="189" r="8" fill='currentColor' />
+        <circle cx="215" cy="169" r="8" fill='currentColor' />
+        <circle cx="236" cy="169" r="8" fill='currentColor' />
+        <circle cx="215" cy="189" r="8" fill='currentColor' />
+        <circle cx="236" cy="189" r="8" fill='currentColor' />
+        <circle cx="257" cy="169" r="8" fill='currentColor' />
+        <circle cx="257" cy="189" r="8" fill='currentColor' />
+        <circle cx="296" cy="169" r="8" fill='currentColor' />
+        <circle cx="296" cy="189" r="8" fill='currentColor' />
+        <circle cx="317" cy="169" r="8" fill='currentColor' />
+        <circle cx="317" cy="189" r="8" fill='currentColor' />
+        <circle cx="380" cy="169" r="8" fill='currentColor' />
+        <circle cx="380" cy="189" r="8" fill='currentColor' />
+        <circle cx="401" cy="169" r="8" fill='currentColor' />
+        <circle cx="401" cy="189" r="8" fill='currentColor' />
+        <circle cx="645" cy="167" r="8" fill='currentColor' />
+        <circle cx="666" cy="167" r="8" fill='currentColor' />
+        <circle cx="666" cy="187" r="8" fill='currentColor' />
+        <circle cx="686" cy="167" r="8" fill='currentColor' />
+        <circle cx="686" cy="187" r="8" fill='currentColor' />
+        <circle cx="726" cy="167" r="8" fill='currentColor' />
+        <circle cx="747" cy="167" r="8" fill='currentColor' />
+        <circle cx="747" cy="187" r="8" fill='currentColor' />
+        <circle cx="767" cy="167" r="8" fill='currentColor' />
+        <circle cx="767" cy="187" r="8" fill='currentColor' />
+        <circle cx="8" cy="129" r="8" fill='currentColor' />
+        <circle cx="29" cy="129" r="8" fill='currentColor' />
+        <circle cx="8" cy="149" r="8" fill='currentColor' />
+        <circle cx="29" cy="149" r="8" fill='currentColor' />
+        <circle cx="50" cy="129" r="8" fill='currentColor' />
+        <circle cx="71" cy="129" r="8" fill='currentColor' />
+        <circle cx="92" cy="129" r="8" fill='currentColor' />
+        <circle cx="113" cy="129" r="8" fill='currentColor' />
+        <circle cx="152" cy="149" r="8" fill='currentColor' />
+        <circle cx="173" cy="129" r="8" fill='currentColor' />
+        <circle cx="194" cy="129" r="8" fill='currentColor' />
+        <circle cx="173" cy="149" r="8" fill='currentColor' />
+        <circle cx="296" cy="129" r="8" fill='currentColor' />
+        <circle cx="296" cy="149" r="8" fill='currentColor' />
+        <circle cx="317" cy="129" r="8" fill='currentColor' />
+        <circle cx="317" cy="149" r="8" fill='currentColor' />
+        <circle cx="359" cy="129" r="8" fill='currentColor' />
+        <circle cx="380" cy="129" r="8" fill='currentColor' />
+        <circle cx="380" cy="149" r="8" fill='currentColor' />
+        <circle cx="401" cy="149" r="8" fill='currentColor' />
+        <circle cx="645" cy="127" r="8" fill='currentColor' />
+        <circle cx="645" cy="147" r="8" fill='currentColor' />
+        <circle cx="666" cy="127" r="8" fill='currentColor' />
+        <circle cx="666" cy="147" r="8" fill='currentColor' />
+        <circle cx="726" cy="127" r="8" fill='currentColor' />
+        <circle cx="726" cy="147" r="8" fill='currentColor' />
+        <circle cx="747" cy="127" r="8" fill='currentColor' />
+        <circle cx="747" cy="147" r="8" fill='currentColor' />
+        <circle cx="8" cy="89" r="8" fill='currentColor' />
+        <circle cx="29" cy="89" r="8" fill='currentColor' />
+        <circle cx="8" cy="109" r="8" fill='currentColor' />
+        <circle cx="29" cy="109" r="8" fill='currentColor' />
+        <circle cx="50" cy="109" r="8" fill='currentColor' />
+        <circle cx="71" cy="109" r="8" fill='currentColor' />
+        <circle cx="92" cy="89" r="8" fill='currentColor' />
+        <circle cx="113" cy="89" r="8" fill='currentColor' />
+        <circle cx="92" cy="109" r="8" fill='currentColor' />
+        <circle cx="113" cy="109" r="8" fill='currentColor' />
+        <circle cx="194" cy="109" r="8" fill='currentColor' />
+        <circle cx="215" cy="89" r="8" fill='currentColor' />
+        <circle cx="236" cy="89" r="8" fill='currentColor' />
+        <circle cx="215" cy="109" r="8" fill='currentColor' />
+        <circle cx="296" cy="89" r="8" fill='currentColor' />
+        <circle cx="296" cy="109" r="8" fill='currentColor' />
+        <circle cx="317" cy="89" r="8" fill='currentColor' />
+        <circle cx="338" cy="89" r="8" fill='currentColor' />
+        <circle cx="317" cy="109" r="8" fill='currentColor' />
+        <circle cx="338" cy="109" r="8" fill='currentColor' />
+        <circle cx="359" cy="89" r="8" fill='currentColor' />
+        <circle cx="380" cy="89" r="8" fill='currentColor' />
+        <circle cx="359" cy="109" r="8" fill='currentColor' />
+        <circle cx="380" cy="109" r="8" fill='currentColor' />
+        <circle cx="401" cy="89" r="8" fill='currentColor' />
+        <circle cx="645" cy="87" r="8" fill='currentColor' />
+        <circle cx="645" cy="107" r="8" fill='currentColor' />
+        <circle cx="666" cy="87" r="8" fill='currentColor' />
+        <circle cx="666" cy="107" r="8" fill='currentColor' />
+        <circle cx="726" cy="87" r="8" fill='currentColor' />
+        <circle cx="726" cy="107" r="8" fill='currentColor' />
+        <circle cx="747" cy="87" r="8" fill='currentColor' />
+        <circle cx="747" cy="107" r="8" fill='currentColor' />
+        <path d="M522 2.5V32.5L454 41.5L522 2.5Z" fill='currentColor' />
+        <path d="M470 134L512.5 185.5L446 146.5L470 134Z" fill='currentColor' />
+        <path d="M582.5 134L540 185.5L606.5 146.5L582.5 134Z" fill='currentColor' />
+        <path d="M467 129L442 142.5L442.5 62L467 129Z" fill='currentColor' />
+        <path d="M519 39.5L472 122.5L445 50L519 39.5Z" fill='currentColor' />
+        <path d="M574 125.5H478L525.5 42.5L574 125.5Z" fill='currentColor' />
+        <path d="M574 133H478L526 190.5L574 133Z" fill='currentColor' />
+        <path d="M533 40L580 123L607 50.5L533 40Z" fill='currentColor' />
+        <path d="M585 129L610 142.5L609.5 62L585 129Z" fill='currentColor' />
+        <path d="M530 3V33L598 42L530 3Z" fill='currentColor' />
+      </svg>
+    </SvgIcon>
+  )
+}
